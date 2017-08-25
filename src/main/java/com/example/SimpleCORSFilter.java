d48751c07a0bb5b737422ff0bdac27595d7dd28f
LAPTOP-MTIQEU28\qijunbo <junboqi@foxmail.com>
2017-08-25 17:43:54 +0800

    allow HTTP PATCH request

--- src/main/java/com/example/SimpleCORSFilter.java
+++ src/main/java/com/example/SimpleCORSFilter.java
@@ -19,7 +19,7 @@ public class SimpleCORSFilter implements Filter {
 		HttpServletResponse response = (HttpServletResponse) res;
 		response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 		response.setHeader("Access-Control-Allow-Origin", "*");
-		response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
+		response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, PATCH");
 		response.setHeader("Access-Control-Max-Age", "3600");
 		chain.doFilter(req, res);
 	}
